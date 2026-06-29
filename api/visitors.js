a209b6377dd59f2844ba112bd40d19b9e2678d55
MakarandDeore3009 <deoremakarand@gmail.com>
2026-06-29 17:31:17 +0530

    Fix Upstash environment variables

--- api/visitors.js
+++ api/visitors.js
@@ -1,21 +1,30 @@
 import { Redis } from "@upstash/redis";
 
-const redis = Redis.fromEnv();
+const redis = new Redis({
+  url: process.env.KV_REST_API_URL,
+  token: process.env.KV_REST_API_TOKEN,
+});
 
 export default async function handler(req, res) {
-
+  try {
     let visitors = await redis.get("visitors");
 
     if (!visitors) {
-        visitors = 0;
+      visitors = 0;
     }
 
     visitors = Number(visitors) + 1;
 
     await redis.set("visitors", visitors);
 
-    res.status(200).json({
-        visitors
+    return res.status(200).json({
+      visitors,
     });
+  } catch (error) {
+    console.error(error);
 
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
 }
